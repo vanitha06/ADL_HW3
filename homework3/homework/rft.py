44502337dda02ea6d8bd52b677b82eaeb21bae6e
vanitha06 <vinuharan@yahoo.co.in>
2026-03-18 23:18:52 +0000

    rft grader timing out

--- homework3/homework/rft.py
+++ homework3/homework/rft.py
@@ -1,5 +1,6 @@
 from .base_llm import BaseLLM
 from .sft import test_model
+from .data import Dataset
 
 
 def load() -> BaseLLM:
@@ -15,6 +16,63 @@ def load() -> BaseLLM:
     llm.model.eval()
 
     return llm
+def format_example(prompt: str, reason: str,answer:str) -> dict[str, str]:
+    """
+    Construct a question / answer pair. Consider rounding the answer to make it easier for the LLM.
+    """
+    # try:
+    #     # Round to 3 decimal places for consistency
+    #     rounded_answer = round(float(answer), 3)
+    # except (ValueError, TypeError):
+    #     rounded_answer = answer
+    # prompt = prompt+"wrap the answer in answer tags"
+    return {"question":prompt,"answer": answer,"reasoning":reason}
+def tokenize(tokenizer, question: str,reasoning:str, answer: str):
+    """
+    Tokenize a data element.
+    We first append the <EOS> token to the question / answer pair.
+    Then we tokenize and construct the ground truth `labels`.
+    `labels[i] == -100` for the question or masked out parts, since we only want to supervise
+    the answer.
+    """
+    full_text = f"{question} {answer}{reasoning}{tokenizer.eos_token}"
+
+    tokenizer.padding_side = "right"
+    tokenizer.pad_token = tokenizer.eos_token
+    full = tokenizer(full_text, padding="max_length", truncation=True, max_length=128)
+
+    input_ids = full["input_ids"]
+    question_len = len(tokenizer(question)["input_ids"])
+
+    # Create labels: mask out the prompt part
+    labels = [-100] * question_len + input_ids[question_len:]
+
+    for i in range(len(labels)):
+        if full["attention_mask"][i] == 0:
+            labels[i] = -100
+
+    full["labels"] = labels
+    return full    
+class TokenizedDataset:
+    def __init__(self, tokenizer, data: Dataset, format_fn):
+        """
+        Use the
+        - BaseLLM.tokenizer
+        - Dataset
+        - format_fn which converts a data element into a dict with entries
+          - question: str
+          - answer: str
+        """
+        self.format_fn = format_fn
+        self.tokenizer = tokenizer
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        formated_data = self.format_fn(*self.data[idx])
+        return tokenize(self.tokenizer, **formated_data)
 
 
 def train_model(
